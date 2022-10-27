313e7194a5ffda001d6c8d819bc45870d4b3b0cc
MildRinrada <mildppgd@gmail.com>
2022-10-28 01:10:41 +0700

    MildRinrada

---
+++ controller/endpoint.js
@@ -0,0 +1,63 @@
+const { Logic } = require("./logic");
+const models = require("../model/model");
+
+class Endpoint {
+  constructor() {
+    this.dataCpu = models.dataCpuModel;
+  }
+
+  //เพิ่มลงใน database
+  createEndpoint = (req, res) => {
+    this.dataCpu.id = req.body.id;
+    this.dataCpu.time_cpu = req.body.time_cpu;
+    this.dataCpu.core_cpu = req.body.core_cpu;
+    new Logic().createLogic(this.dataCpu, res);
+  };
+
+  //ลบแถวใน Database
+  deleteEndpoint = (req, res) => {
+    this.dataCpu.id = req.params.id;
+    new Logic().deleteLogic(this.dataCpu, res);
+  };
+
+  //รับข้อมูลทุกอันใน Database
+  getEndpoint = (req, res) => {
+    new Logic().getLogic(res);
+  };
+
+  //รับข้อมูลบางอันใน Database
+  getUserCpuEndpoint = (req, res) => {
+    this.dataCpu.id = req.body.id;
+    new Logic().getUserCpuLogic(this.dataCpu, res);
+  };
+
+  //แก้ไขข้อมูลใน Database
+  updateEndpoint = (req, res) => {
+    this.dataCpu.id = req.body.id;
+    this.dataCpu.time_cpu = req.body.time_cpu;
+    this.dataCpu.core_cpu = req.body.core_cpu;
+    new Logic().updateLogic(this.dataCpu, res);
+  };
+
+  //รับข้อมูลทุกอันใน Database เรียงลำดับค่ามากสุดมาก่อน
+  getDescEndpoint = (req, res) => {
+    new Logic().getDescLogic(res);
+  };
+
+  //รับข้อมูลทุกอันใน Database เรียงลำดับค่าน้อยสุดมาก่อน
+  getAscEndpoint = (req, res) => {
+    new Logic().getAscLogic(res);
+  };
+
+  //แสดงผลลัพธ์ค่าที่มากที่สุด
+  getMaxEndpoint = (req, res) => {
+    new Logic().getMaxLogic(res);
+  };
+
+  // ViralMemeEndpoint = (req, res) => {
+  //    new Logic().getViralMemeLogic(res);
+  // }
+}
+module.exports = {
+  Endpoint,
+};
---
+++ router/router.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const connection = require("../database.connect/connector");
+
+const { Endpoint } = require("../controller/endpoint.js");
+
+//สร้างแถว
+router.post("/createCpu", new Endpoint().createEndpoint);
+
+//ลบแถว
+router.post("/deleteCpu/:id", new Endpoint().deleteEndpoint);
+
+//แก้ไขข้อมูล
+router.put("/updateCpu", new Endpoint().updateEndpoint);
+
+//รับข้อมูล Record ทั้งหมด
+router.get("/getCpu", new Endpoint().getEndpoint);
+
+//รับข้อมูลอันนึง
+router.get("/getUserCpu", new Endpoint().getUserCpuEndpoint);
+
+//รับข้อมูล Record เรียงลำดับมากไปน้อย
+router.get("/getDesc", new Endpoint().getDescEndpoint);
+
+//รับข้อมูล Record เรียงลำดับน้อยไปมาก
+router.get("/getAsc", new Endpoint().getAscEndpoint);
+
+//แสดงค่าสูงสุด
+router.get("/getMax", new Endpoint().getMaxEndpoint);
+
+router.get("/", function (req, res) {
+  res.render("index", { title: "MVC 63050176" });
+});
+
+module.exports = router;
